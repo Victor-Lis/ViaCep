7699e3075381b9537bcad1a8974489cd4f08e6b0
Victor-Lis <victorlisbronzo1@gmail.com>
2022-12-30 20:36:45 -0300

    v1

--- JS/script.js
+++ JS/script.js
@@ -1,69 +1,28 @@
-const estadoInput = document.querySelector("#input-estado")
-const cidadeInput = document.querySelector("#input-cidade")
-const ruaInput = document.querySelector("#input-rua")
+const main = document.getElementById("main")
 
-const answerArea = document.querySelector("#answer-area")
-answerArea.style.display = "none"
+async function enviar(){
 
-var fullScreenTrack = "full screen off";
+    var estado = document.getElementById("estado").value
+    var cidade = document.getElementById("cidade").value
+    var rua = document.getElementById("rua").value
 
-const answerAreaElements = {
+    var resposta = await fetch(`https://viacep.com.br/ws/${estado}/${cidade}/${rua}/json/`)
 
-    estado: document.querySelector("#estado"),
-    localidade: document.querySelector("#localidade"),
-    bairro: document.querySelector("#bairro"),
-    logradouro: document.querySelector("#logradouro"),    
-    cep: document.querySelector("#cep"),
-    ddd: document.querySelector("#ddd")
+    var dados = await resposta.json()
 
-}
+    document.getElementById("respostas").style.display = "flex"
 
-async function getData(){
+    main.style.height = "auto"
 
-    if(estadoInput.value == "" || cidadeInput.value == "" || ruaInput.value == ""){
+    console.log(main.style.height)
 
-        window.navigator.vibrate(200,10,200);
+    console.log(dados)
 
-        setTimeout(() => {
-
-            alert("Preencha todos os campos!")
-
-        }, 300)
-
-    }
-
-    const response = await fetch(`https://viacep.com.br/ws/${estadoInput.value}/${cidadeInput.value}/${ruaInput.value}/json/`)
-
-    const data = await response.json()
-
-    console.log(data[0])
-
-    answerAreaElements.estado.innerText = data[0].uf
-    answerAreaElements.localidade.innerText = data[0].localidade
-    answerAreaElements.bairro.innerText = data[0].bairro
-    answerAreaElements.logradouro.innerText = data[0].logradouro
-    answerAreaElements.cep.innerText = data[0].cep
-    answerAreaElements.ddd.innerText = data[0].ddd
-
-    answerArea.style.display = "inherit"
-
-    fullScreenTrack = "full screen on"
-    answerArea.requestFullscreen()
+    document.getElementById("uf").innerText = dados[0].uf
+    document.getElementById("localidade").innerText = dados[0].localidade
+    document.getElementById("bairro").innerText = dados[0].bairro
+    document.getElementById("cep").innerText = dados[0].cep
+    document.getElementById("ddd").innerText = dados[0].ddd
+    document.getElementById("logradouro").innerText = dados[0].logradouro
 
 }
-
-answerArea.addEventListener("click", () => {
-
-    if(fullScreenTrack == "full screen off"){
-
-        fullScreenTrack = "full screen on"
-        answerArea.requestFullscreen()
-
-    }else if(fullScreenTrack == "full screen on"){
-
-        fullScreenTrack = "full screen off"
-        document.exitFullscreen()
-
-    }
-
-})
