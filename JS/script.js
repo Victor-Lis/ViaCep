581b2469768f2a437024d28e1a3483563f73001e
Victor-Lis <victorlisbronzo1@gmail.com>
2023-11-06 11:04:46 -0300

    v2

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,116 @@
 const main = document.getElementById("main")
 
-async function enviar(){
+async function enviar() {
 
     var estado = document.getElementById("estado").value
     var cidade = document.getElementById("cidade").value
     var rua = document.getElementById("rua").value
 
-    var resposta = await fetch(`https://viacep.com.br/ws/${estado}/${cidade}/${rua}/json/`)
+    if (!!estado || !!cidade || !!rua) {
 
-    var dados = await resposta.json()
+        var resposta = await fetch(`https://viacep.com.br/ws/${estado}/${cidade}/${rua}/json/`)
 
-    document.getElementById("respostas").style.display = "flex"
+        var dados = await resposta.json()
 
-    main.style.height = "auto"
+        main.style.height = "auto"
 
-    console.log(main.style.height)
+        main.innerHTML = `
+    
+        <div class="perguntas"> 
+            
+            <h2> Preencha os dados </h2>
 
-    console.log(dados)
+            <div class="inputs">
 
-    document.getElementById("uf").innerText = dados[0].uf
-    document.getElementById("localidade").innerText = dados[0].localidade
-    document.getElementById("bairro").innerText = dados[0].bairro
-    document.getElementById("cep").innerText = dados[0].cep
-    document.getElementById("ddd").innerText = dados[0].ddd
-    document.getElementById("logradouro").innerText = dados[0].logradouro
+                <select id="estado" placeholder="Estado (Sigla)">
+                </select>
+                <input id="cidade" placeholder="Cidade" maxlength="10"></input>
+                <input id="rua" placeholder="Rua" maxlength="30"></input>
+
+            </div>
+
+            <button onclick="enviar()"> Enviar! </button>
+
+        </div>
+
+    `
+
+        preencherSelect()
+
+        dados.map(dado => {
+
+        main.innerHTML +=
+                `
+        <div class="respostas" id="respostas"> 
+
+            <h2> Informações </h2>
+
+            <div class="cards">
+
+                <div class="card">
+
+                    <h3> Estado </h3>
+                    <p id="uf"> ${dado.uf} </p> 
+
+                </div>
+
+                <div class="card">
+
+                    <h3> Localidade </h3>
+                    <p id="localidade"> ${dado.localidade} </p>
+
+                </div>
+
+                <div class="card">
+
+                    <h3> Bairro </h3>
+                    <p id="bairro"> ${dado.bairro} </p>
+
+                </div>
+        
+                <div class="card"  style="order: 1">
+
+                    <h3> Logradouro </h3>
+                    <p id="logradouro"> ${dado.logradouro} </p>
+
+                </div>
+
+                <div class="card"  style="order: 0">
+
+                    <h3> CEP </h3>
+                    <p id="cep"> ${dado.cep} </p>
+
+                </div>
+
+                <div class="card" style="order: 2">
+
+                    <h3> DDD </h3>
+                    <p id="ddd"> ${dado.ddd} </p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        `
+
+        })
+
+    }
 
 }
+
+function preencherSelect() {
+
+    let estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"]
+    let sigla = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"];
+    estados.map((estado, index) => {
+
+        document.getElementById("estado").innerHTML += `<option value="${sigla[index]}">${estados[index]}</option>`
+
+    })
+
+}
+
+preencherSelect()
